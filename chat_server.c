5ed4592c2a4eaa41836003cba9c515fdc00f9787
Jeta-Syla <117122719+Jeta-Syla@users.noreply.github.com>
2023-05-30 14:50:05 +0200

    Update chat_server.c

--- chat_server.c
+++ chat_server.c
@@ -18,3 +18,28 @@ typedef struct {
 Client clients[MAX_CLIENTS];
 int client_count = 0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void broadcast_message(char* sender_name, char* message) {
+    char broadcast_buffer[BUFFER_SIZE];
+    snprintf(broadcast_buffer, BUFFER_SIZE, "%s: %s", sender_name, message);
+
+    pthread_mutex_lock(&mutex);
+    for (int i = 0; i < client_count; i++) {
+        write(clients[i].socket, broadcast_buffer, strlen(broadcast_buffer) + 1);
+    }
+    pthread_mutex_unlock(&mutex);
+}
+
+void send_private_message(char* sender_name, char* recipient_name, char* message) {
+    char private_buffer[BUFFER_SIZE];
+    snprintf(private_buffer, BUFFER_SIZE, "[Mesazh privat nga %s për %s]: %s", sender_name, recipient_name, message);
+
+    pthread_mutex_lock(&mutex);
+    for (int i = 0; i < client_count; i++) {
+        if (strcmp(clients[i].name, recipient_name) == 0) {
+            write(clients[i].socket, private_buffer, strlen(private_buffer) + 1);
+            break;
+        }
+    }
+    pthread_mutex_unlock(&mutex);
+}
