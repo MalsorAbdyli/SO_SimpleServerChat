905fc02532c4cfbacc0efac6c29f8db4a62d5429
MalsorAbdyli <122939291+MalsorAbdyli@users.noreply.github.com>
2023-05-30 15:06:13 +0200

    Update chat_server.c

--- chat_server.c
+++ chat_server.c
@@ -141,3 +141,77 @@ void* handle_client(void* client_socket_ptr) {
         handle_client_message(current_client_count, client_message);
     }
 }
+int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_addr_len;
+    pthread_t client_threads[MAX_CLIENTS];
+
+    // krijo nje socket
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        perror("Dështoi për të krijuar soketat");
+        exit(1);
+    }
+
+    // vendose adresen e serverit
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(8888);
+
+    // lidhe socketin
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Dështoi për të lidhur soketat");
+        exit(1);
+    }
+
+    // shiko per connections
+    if (listen(server_socket, MAX_CLIENTS) < 0) {
+        perror("Dështoi për të marrë lidhjet");
+        exit(1);
+    }
+
+    printf("Serveri filloi. Duke pritur për lidhje...\n");
+
+    while (1) {
+        // prano connection prej klientit
+        client_addr_len = sizeof(client_addr);
+        client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_len);
+        if (client_socket < 0) {
+            perror("Dështoi pranimi i kërkesës së lidhjes nga klienti");
+            continue;
+        }
+
+        if (client_count == MAX_CLIENTS) {
+            printf("Maksimumi i klientëve është arritur. Lidhja refuzohet.\n");
+            close(client_socket);
+            continue;
+        }
+
+        // cakto memorien per thread argumentin e klientit
+        int* client_socket_ptr = malloc(sizeof(int));
+        if (client_socket_ptr == NULL) {
+            perror("Dështoi përcaktimi i memories për pointerin e soketës së klientit.");
+            close(client_socket);
+            continue;
+        }
+
+        // vendose vleren e socketit te klientit
+        *client_socket_ptr = client_socket;
+
+        clients[client_count].socket = client_socket;
+
+        // krijo threadin per klientin
+        if (pthread_create(&client_threads[client_count], NULL, handle_client, client_socket_ptr) != 0) {
+            perror("Dështoi krijimi i thread-it të klientit");
+            close(client_socket);
+            free(client_socket_ptr);
+            continue;
+        }
+    }
+
+    // mbylle socket serverin
+    close(server_socket);
+
+    return 0;
+}
